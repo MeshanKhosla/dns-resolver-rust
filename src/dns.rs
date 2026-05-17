625aa0a16557ae25bac72e317a4a30b7978019bf
Meshan Khosla <mkhosla@berkeley.edu>
2026-05-17 11:48:22 -0700

    Dns Question format

--- src/dns.rs
+++ src/dns.rs
@@ -128,6 +128,24 @@ struct DnsHeader {
 }
 
 #[derive(Debug)]
-struct DnsQuestion {}
+struct DnsQuestion {
+    /// A domain name represented as a sequence of labels, where
+    /// each label consists of a length octet followed by that
+    /// number of octets.  The domain name terminates with the
+    /// zero length octet for the null label of the root.  Note
+    /// that this field may be an odd number of octets; no
+    /// padding is used.
+    qname: u16,
+
+    /// A two octet code which specifies the type of the query.
+    /// The values for this field include all codes valid for a
+    /// TYPE field, together with some more general codes which
+    /// can match more than one type of RR.
+    qtype: u16,
+
+    /// A two octet code that specifies the class of the query.
+    /// For example, the QCLASS field is IN for the Internet.
+    qclass: u16,
+}
 #[derive(Debug)]
 struct ResourceRecord {}
